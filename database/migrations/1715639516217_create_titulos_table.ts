439de51d96022431b5234849a16a66de28640d60
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-05-15 20:14:18 -0300

    Apagando os seeders para refazer

--- database/migrations/1715639516217_create_titulos_table.ts
+++ database/migrations/1715639516217_create_titulos_table.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('titulos', 255).notNullable()
-      table.string('descrição').nullable()
+      table.string('nome', 255).notNullable()
+      table.string('descricao').nullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
