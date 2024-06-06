76761098dbb623815d184b6218ef37ce33533667
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-06-06 20:34:29 -0300

    Projeto finalizado

--- database/seeders/7_personagen_seeder.ts
+++ database/seeders/7_personagen_seeder.ts
@@ -5,7 +5,7 @@ export default class extends BaseSeeder {
   async run() {
     
     await Personagen.createMany([
-      { nome: "Naruto Uzumaki", idade: "17", aldeiaId: 1, rankNum: "Hokage", missao_atual: "Proteger a vila", status: "Ativo" },
+      { nome: "Naruto Uzumaki", idade: "17", aldeiaId: 1, rankNum: "Hokage", missao_atual: "Proteger a vila", status: "Ativo", tituloId: 1},
       { nome: "Sasuke Uchiha", idade: "17", aldeiaId: 1, rankNum: "Nukenin", missao_atual: "Buscar poder", status: "Ativo" },
     ])
 
